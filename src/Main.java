b6196e23b20fcab6784cc342bccaac9f8f9b741c
maks <mamonov.maksim.20003@gmail.com>
2022-09-21 18:15:42 +0500

    второе задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
-        Human Maksim = new Human(1987, "Максим", "Минск");
-        Human Ania = new Human(1993, "Аня", "Москва");
-        Human Katia = new Human(1994, "Катя", "калининград");
-        Human Artiem = new Human(1995, "Артём", "Москва");
+        Human Maksim = new Human(1987, "Максим", "Минск", "бренд менеджером");
+        Human Ania = new Human(1993, "Аня", "Москва", "методистом");
+        Human Katia = new Human(1994, "Катя", "калининград", "продакт менеджером");
+        Human Artiem = new Human(1995, "Артём", "Москва", "директором по развитию бизнеса");
         Maksim.Human();
         Ania.Human();
         Katia.Human();
