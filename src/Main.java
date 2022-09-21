8fd954e3ea50d0e2dc86d4ec94a8fa49f85c5971
maks <mamonov.maksim.20003@gmail.com>
2022-09-21 18:08:27 +0500

    первое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        Human Maksim = new Human(1987, "Максим", "Минск");
+        Human Ania = new Human(1993, "Аня", "Москва");
+        Human Katia = new Human(1994, "Катя", "калининград");
+        Human Artiem = new Human(1995, "Артём", "Москва");
+        Maksim.Human();
+        Ania.Human();
+        Katia.Human();
+        Artiem.Human();
     }
 }
