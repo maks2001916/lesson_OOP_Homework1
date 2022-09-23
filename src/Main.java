64ee069e20f9ea841e3c0832e61b6898a3a29678
maks <mamonov.maksim.20003@gmail.com>
2022-09-23 17:10:05 +0500

    первое задание зретьего дз выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args)
     {
-        Human Maksim = new Human(1987, "Максим", "Минск", "бренд менеджером");
+        Human Maksim = new Human(1987,"Максим", "Минск", "бренд менеджером");
         Human Ania = new Human(1993, "Аня", "Москва", "методистом");
         Human Katia = new Human(1994, "Катя", "калининград", "продакт менеджером");
         Human Artiem = new Human(1995, "Артём", "Москва", "директором по развитию бизнеса");
+
         Maksim.Human();
         Ania.Human();
         Katia.Human();
         Artiem.Human();
+
     }
 }
